41f8e1bb745a5427e757ee3ce6b2e9730580df2e
Dmitry Novosyolov <i1710@icloud.com>
2023-01-17 21:59:31 +0200

    Initial Commit

---
+++ Custom Calculator SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Custom Calculator SwiftUI
+//
+//  Created by Dmitry Novosyolov on 17/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Custom Calculator SwiftUI/Custom_Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Calculator_SwiftUIApp.swift
+//  Custom Calculator SwiftUI
+//
+//  Created by Dmitry Novosyolov on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
